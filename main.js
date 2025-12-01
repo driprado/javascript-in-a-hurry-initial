be8bbc17e2650895ac097af1c521b4dea1a37fc1
Adria Prado <adriabp.br@gmail.com>
2025-12-01 11:11:03 +1100

    Arrays

--- main.js
+++ main.js
@@ -5,7 +5,28 @@ const weatherCondition = "Sunny"
 const userLocation = "New York"
 let temperature = "22.8673"
 // ${temperature.toFixed(1)} round to one decimal place
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`
+let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${Number(temperature).toFixed(1)}°C outside.`
 
 document.querySelector("#greeting").innerHTML = greetingText
 document.querySelector("#weather").innerHTML = weatherText
+
+// Arrays
+// get last element of an array:
+let array1 = [2, 4, 6, 8]
+console.log(array1[array1.length -1])
+
+// add item to end of array: push()
+array1.push(10)
+console.log(array1[array1.length -1])
+console.log(array1)
+
+// add item to begigging of array: unshift()
+array1.unshift(0)
+console.log(array1[array1.length -1])
+console.log(array1)
+
+// join arrays: concat()
+array2 = [12,14,16]
+array3 = array1.concat(array2)
+console.log(array1[array3.length -1])
+console.log(array3)
